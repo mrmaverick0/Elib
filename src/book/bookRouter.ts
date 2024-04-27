9ae18726e5c31883af9888ef05707d3efb3d5c82
Mohd Umar <ua1411978@gmail.com>
2024-04-27 10:25:39 +0530

    setup multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import path from "node:path"
 import createBook from "./bookController";
+import multer from "multer";
 
 const bookRouter = express.Router();
 
+//! multer -> file store local(temp) -> upload cloudnary then delete local file
+const upload = multer({
+    dest:path.resolve(__dirname,'../../public/data/uploads'),
+    limits:{fileSize:3e7} //todo 30MB
+})
+
 //router
-bookRouter.post('/register',createBook);
+bookRouter.post('/',upload.fields([
+    {name:'coverImage',maxCount:1},
+    {name:'file',maxCount:1},
+]),createBook);
 export default bookRouter;
